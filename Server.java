9829ae2beac81e00b92c99d8ec329d5a1af30c50
rlaxton <rlaxton@sfu.ca>
2019-01-20 15:35:50 -0800

    Adding files

---
+++ Server.java
@@ -0,0 +1,24 @@
+import java.rmi.registry.LocateRegistry;
+import java.rmi.registry.Registry;
+import java.rmi.server.UnicastRemoteObject;
+
+public class Server extends ImplServerDate{
+
+    public Server(){}
+
+    public static void main(String[] args) {
+        try{
+            int port = 1099;
+            System.setProperty("java.rmi.server.hostname","10.0.75.1");
+            ImplServerDate obj = new ImplServerDate();
+            Registry registry = LocateRegistry.createRegistry(port);
+            ServerDate stub = (ServerDate) UnicastRemoteObject.exportObject(obj, port);
+
+            registry.bind("CurrentDate", stub);
+            System.err.println("Server Ready");
+        } catch(Exception e){
+            System.err.println("Server exception: " + e.toString());
+            e.printStackTrace();
+        }
+    }
+}
